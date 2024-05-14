da567ff26953628c8c5a9295ebbb8eb276397eda
Dawit Zewdu <106137900+dawit2123@users.noreply.github.com>
2024-05-14 17:29:34 +0300

    Time: 54 ms (66.40%) | Memory: 51.9 MB (24.72%) - LeetSync

--- 2798-chunk-array/chunk-array.js
+++ 2798-chunk-array/chunk-array.js
@@ -6,11 +6,7 @@
 var chunk = function(arr, size) {
     let finalArray=[];
     for(let i=0; i<arr.length; i+=size){
-        if(i+size>arr.length){
-            finalArray.push(arr.slice(i,arr.length));
-        }else{
             finalArray.push(arr.slice(i,i+size));
-        }
     }
     return finalArray;
 };
