838849fd73c3db342bddb516bc13f4baf03f3434
Dawit Zewdu <106137900+dawit2123@users.noreply.github.com>
2024-08-31 22:44:29 +0300

    Time: 42 ms (69.60%) | Memory: 16.7 MB (42.02%) - LeetSync

--- 682-baseball-game/baseball-game.py
+++ 682-baseball-game/baseball-game.py
@@ -5,12 +5,9 @@ class Solution:
             if(value=="C"):
                 stack.pop()
             elif(value=="D"):
-                stack.append(int(stack[len(stack)-1])*2)
+                stack.append(stack[-1]*2)
             elif(value=="+"):
-                stack.append(int(stack[len(stack)-1])+ int(stack[len(stack)-2]))
+                stack.append(stack[-1]+ stack[-2])
             else:
-                stack.append(value)
-        sum=0
-        for result in stack:
-            sum+=int(result)
-        return sum
+                stack.append(int(value))
+        return sum(stack)
