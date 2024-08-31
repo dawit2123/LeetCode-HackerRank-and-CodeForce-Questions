5d5c197b74a965e570ba9886101c0d1a971d5902
Dawit Zewdu <106137900+dawit2123@users.noreply.github.com>
2024-08-31 21:29:40 +0300

    Time: 54 ms (61.47%) | Memory: 16.8 MB (11.00%) - LeetSync

--- 1580-shuffle-the-array/shuffle-the-array.py
+++ 1580-shuffle-the-array/shuffle-the-array.py
@@ -1,16 +1,7 @@
 class Solution:
     def shuffle(self, nums: List[int], n: int) -> List[int]:
-        l=0
-        r=n
-        is_left=True
         numbers=[]
-        for i in range(0,len(nums)):
-            if(is_left==True):
-                numbers.append(nums[l])
-                l+=1
-                is_left=False
-            else:
-                numbers.append(nums[r])
-                r+=1
-                is_left=True
+        for i in range(0,n):
+            numbers.append(nums[i])
+            numbers.append(nums[i+n])
         return numbers
