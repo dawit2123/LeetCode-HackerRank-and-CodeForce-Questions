f49db4fc1518739eee8caf0e4a31bd503d79921e
Dawit Zewdu <106137900+dawit2123@users.noreply.github.com>
2024-10-03 14:41:36 +0300

    Time: 368 ms (71.52%) | Memory: 18.4 MB (85.01%) - LeetSync

---
+++ 744-network-delay-time/network-delay-time.py
@@ -0,0 +1,22 @@
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        attributes={}
+        for i in range(1, n+1):
+            attributes[i]=[]
+        # iterate over the times and fill in the attributes
+        for s,d,w in times:
+            attributes[s].append((d,w))
+        time_travel_result={}
+        #instantiate a min_heap
+        min_heap=[(0,k)]
+        while min_heap:
+            w1, n1, = heapq.heappop(min_heap)
+            if n1 in time_travel_result:
+                continue
+            time_travel_result[n1]=w1
+
+            # add all the adjacents of the current into the min_heap
+            for n2, w2 in attributes[n1]:
+                if n2 not in time_travel_result:
+                    heapq.heappush(min_heap, ((w2+w1), n2))
+        return max(time_travel_result.values()) if len(time_travel_result)==n else -1
