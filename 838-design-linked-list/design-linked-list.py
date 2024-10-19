ebaaa95453200c231d2bd8d9583dbba4a273e3fc
Dawit Zewdu <106137900+dawit2123@users.noreply.github.com>
2024-10-19 11:40:25 +0300

    Time: 35 ms (100.00%) | Memory: 18 MB (32.30%) - LeetSync

--- 838-design-linked-list/design-linked-list.py
+++ 838-design-linked-list/design-linked-list.py
@@ -1,71 +1,70 @@
 class Node:
-    def __init__(self, val=0, next=None):
-        self.val=val
-        self.next=next
+        def __init__(self, val=0, left=None, right=None):
+            self.val=val
+            self.left=left
+            self.right=right
 class MyLinkedList:
 
     def __init__(self):
-        self.head=None
+        self.left_boundary, self.right_boundary=Node(), Node()
+        self.left_boundary.right=self.right_boundary
+        self.right_boundary.left=self.left_boundary
 
     def addAtHead(self, val: int) -> None:
-        if not self.head:
-            self.head=Node(val)
-            return
-        node=Node(val, self.head)
-        self.head=node
+        node, next, prev= Node(val), self.left_boundary.right, self.left_boundary
+        node.right=next
+        node.left=prev
+        prev.right=node
+        next.left=node
 
     def get(self, index: int) -> int:
-        i=0 
-        cur=self.head
+        i=0
+        cur=self.left_boundary.right
         while cur:
-            print(cur.val)
-            if i==index:
+            if i==index and cur!=self.right_boundary:
                 return cur.val
             else:
                 i+=1
-                cur=cur.next
+                cur=cur.right
         return -1
-
+        
     def addAtTail(self, val: int) -> None:
-        cur=self.head
-        if not self.head:
-            self.head=Node(val)
-            return
-        while cur.next:
-            cur=cur.next
-        node=Node(val)
-        cur.next=node
+        node, prev, next=Node(val), self.right_boundary.left, self.right_boundary
+        node.right=next
+        node.left=prev
+        prev.right=node
+        next.left=node
+
         
     def addAtIndex(self, index: int, val: int) -> None:
-       i=0
-       cur=self.head
-       if index==0:
-        self.head=Node(val)
-        self.head.next=cur
-       while cur:
-        i+=1
-        if i==index:
-            node=Node(val)
-            node.next=cur.next
-            cur.next=node
-            return
-        cur=cur.next
-
+        i=0
+        cur=self.left_boundary.right
+        while cur:
+            if i==index:
+                node, prev, next= Node(val), cur.left, cur
+                node.right=next
+                node.left=prev
+                prev.right=node
+                next.left=node
+                return
+            else:
+                cur=cur.right
+                i+=1
+            
+       
     def deleteAtIndex(self, index: int) -> None:
         i=0
-        cur=self.head
-        previous_node=None
-        while cur and i!=index:
-            previous_node=cur
-            cur=cur.next
-            i+=1
-        if not cur:
-            return 
-        if index==0:
-            self.head=self.head.next
-            return
-        previous_node.next=previous_node.next.next
-        cur=None
+        cur=self.left_boundary.right
+        while cur:
+            if i==index and cur!=self.right_boundary:
+                prev, next= cur.left, cur.right
+                prev.right=next
+                next.left=prev
+                return
+            else:
+                i+=1
+                cur=cur.right
+        
 # Your MyLinkedList object will be instantiated and called as such:
 # obj = MyLinkedList()
 # param_1 = obj.get(index)
