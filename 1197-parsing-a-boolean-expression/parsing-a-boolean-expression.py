0d4728b79d00e04469d6f5ef59474686eab4553e
Dawit Zewdu <106137900+dawit2123@users.noreply.github.com>
2024-10-20 13:56:20 +0300

    Time: 11 ms (100.00%) | Memory: 16.7 MB (80.84%) - LeetSync

--- 1197-parsing-a-boolean-expression/parsing-a-boolean-expression.py
+++ 1197-parsing-a-boolean-expression/parsing-a-boolean-expression.py
@@ -2,33 +2,24 @@ class Solution:
     def parseBoolExpr(self, expression: str) -> bool:
         stack=[]
         for char in expression:
-            if char == ',':
+            if char in '(,':
                 continue
             elif char in 'tf':
-                # Push True for 't' and False for 'f'
                 stack.append(True if char == 't' else False)
-            elif char in '(&|!':
-                # Push the operator onto the stack
+            elif char in '&|!':
                 stack.append(char)
             elif char == ')':
-                # We've reached the end of an expression, process it
                 operands = []
                 
-                # Pop the stack until we find the operator
                 while stack and (type(stack[-1]) is bool):
                     operands.append(stack.pop())
-                stack.pop()
                 operator = stack.pop()
                 
                 if operator == '&':
-                    # AND: return True only if all operands are True
                     stack.append(all(operands))
                 elif operator == '|':
-                    # OR: return True if any operand is True
                     stack.append(any(operands))
                 elif operator == '!':
-                    # NOT: only one operand is expected, negate it
                     stack.append(not operands[0])
         
-        # The final result will be the only item left on the stack
         return stack[-1]
