f8e08e2774b4b4e912fca7daea5bfd56f653473c
Dawit Zewdu <106137900+dawit2123@users.noreply.github.com>
2024-11-01 19:19:40 +0300

    Time: 7 ms (39.15%) | Memory: 16.7 MB (38.29%) - LeetSync

---
+++ 829-subdomain-visit-count/subdomain-visit-count.py
@@ -0,0 +1,13 @@
+class Solution:
+    def subdomainVisits(self, cpdomains: List[str]) -> List[str]:
+        domains_count=defaultdict(int)
+
+        for domains in cpdomains:
+            count, sub_domains= domains.split(" ")
+            sbs= sub_domains.split('.')
+            for i in range(len(sbs)):
+                domains_count['.'.join(sbs[i:])]+=int(count)
+        res=[]
+        for domain, count in domains_count.items():
+            res.append(str(count)+" "+ domain)
+        return res
