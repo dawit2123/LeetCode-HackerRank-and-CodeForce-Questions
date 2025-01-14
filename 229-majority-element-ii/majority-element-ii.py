4d4d683a06c95becc1bb97ae1426f08074665573
Dawit Zewdu <106137900+dawit2123@users.noreply.github.com>
2025-01-14 22:33:33 +0300

    Time: 4 ms (53.16%) | Memory: 19.3 MB (7.82%) - LeetSync

---
+++ 229-majority-element-ii/majority-element-ii.py
@@ -0,0 +1,9 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        elements_count=Counter(nums)
+        res=[]
+        threshold= len(nums)//3
+        for element , count in elements_count.items():
+            if count > threshold:
+                res.append(element)
+        return res
