6b5fae72c3a4b401c056893ccd823efbf3f775b8
Dawit Zewdu <106137900+dawit2123@users.noreply.github.com>
2025-02-02 18:14:40 +0300

    Time: 0 ms (100.00%) | Memory: 17.9 MB (29.61%) - LeetSync

---
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.py
@@ -0,0 +1,12 @@
+class Solution:
+    def check(self, nums: List[int]) -> bool:
+        n= len(nums)
+        if n<=1:
+            return True
+        inversion_count=0
+        for i in range(1, n):
+            if nums[i]<nums[i-1]:
+                inversion_count+=1
+        if nums[0]<nums[n-1]:
+            inversion_count+=1
+        return inversion_count<=1
