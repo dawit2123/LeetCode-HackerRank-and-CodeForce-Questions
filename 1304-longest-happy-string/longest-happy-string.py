19507483b6c66e7c0c508a4cc5fecdc2714ca854
Dawit Zewdu <106137900+dawit2123@users.noreply.github.com>
2025-02-10 20:20:45 +0300

    Time: 0 ms (100.00%) | Memory: 17.8 MB (40.97%) - LeetSync

--- 1304-longest-happy-string/longest-happy-string.py
+++ 1304-longest-happy-string/longest-happy-string.py
@@ -1,23 +1,25 @@
-class Solution:
-    def longestDiverseString(self, a: int, b: int, c: int) -> str:
-        res, max_heap= "", []
-        for count, char in [(-a, "a"), (-b, "b"), (-c, "c")]:
-            if count!=0:
-                heapq.heappush(max_heap, (count, char))
-        while max_heap:
-            count, char= heapq.heappop(max_heap)
-            if len(res)>1 and res[-1]==res[-2]==char:
-                if not max_heap:
-                    break
-                count2, char2 = heapq.heappop(max_heap)
-                res+=char2
-                count2+=1
-                if count2:
-                    heapq.heappush(max_heap, (count2, char2))
-            else:
-                count+=1
-                res+=char
-            if count:
-                heapq.heappush(max_heap, (count, char))
+import heapq  
+
+class Solution:  
+    def longestDiverseString(self, a: int, b: int, c: int) -> str:  
+        res = ""  
+        max_heap =[]
+        for i, c in [[a,"a"], [b, "b"], [c, "c"]]:
+            if i!=0:
+                max_heap.append([-i, c])
+        heapq.heapify(max_heap)  
+        prev = None
+        while len(max_heap)>0:  
+            val, char = heapq.heappop(max_heap)   
+            val = -1 * val  
+            req_char= min(2, val)
+            print(prev, val)
+            if prev and prev[0]>val:
+                req_char= min(1, val)
+            res+=char*req_char
+            if prev and val > 0:  
+                heapq.heappush(max_heap, [-prev[0], prev[1]])  
+                prev=None
+            val-=req_char
+            prev = [val, char] if val > 0 else [0, ""]  
         return res
-                
