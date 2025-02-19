99c14c2bb6263ad5c44c34def3d85e45a8cc86f9
Dawit Zewdu <106137900+dawit2123@users.noreply.github.com>
2025-02-20 00:53:40 +0300

    Created 'Watermelon' by A2SV Hub

---
+++ 19-Feb-2025/Watermelon 275306.py
@@ -0,0 +1,4 @@
+# Problem: Watermelon - https://codeforces.com/problemset/problem/4/A
+
+watermelon_weight = int(input())
+print("YES" if watermelon_weight > 2 and watermelon_weight % 2 == 0 else "NO")
