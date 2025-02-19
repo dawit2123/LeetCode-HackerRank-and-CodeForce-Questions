dd09c1f1c1a517039369715a03f7cddf498d1381
Dawit Zewdu <106137900+dawit2123@users.noreply.github.com>
2025-02-20 00:06:09 +0300

    Created 'Palindrome Number' by A2SV Hub

---
+++ 19-Feb-2025/Palindrome Number 275265.py
@@ -0,0 +1,9 @@
+# Problem: Palindrome Number - https://leetcode.com/problems/palindrome-number/
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if str(x) == str(x)[::-1]:
+            return True
+        else:
+            return  False  
+            
