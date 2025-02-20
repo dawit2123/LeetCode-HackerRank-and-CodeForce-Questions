db1b829052e13eb0f0fe9d66508414078f005692
Dawit Zewdu <106137900+dawit2123@users.noreply.github.com>
2025-02-20 21:56:10 +0300

    Created 'Transpose Matrix' by A2SV Hub

---
+++ 20-Feb-2025/Transpose Matrix 277325.py
@@ -0,0 +1,11 @@
+# Problem: Transpose Matrix - https://leetcode.com/problems/transpose-matrix/
+
+class Solution:
+    def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
+        result= [[0]*len(matrix) for _ in range(len(matrix[0]))]
+        ROW= len(matrix)
+        COLS= len(matrix[0])
+        for r in range(ROW):
+            for c in range(COLS):
+                result[c][r] = matrix[r][c]
+        return result
