74ef5203ce69b7318f1493920a44f1b3b9e083b0
Dawit Zewdu <106137900+dawit2123@users.noreply.github.com>
2025-02-20 21:42:58 +0300

    Created 'Image Smoother' by A2SV Hub

---
+++ 20-Feb-2025/Image Smoother 277303.py
@@ -0,0 +1,16 @@
+# Problem: Image Smoother - https://leetcode.com/problems/image-smoother/description/
+
+class Solution:
+    def imageSmoother(self, img: List[List[int]]) -> List[List[int]]:
+        result_img= [[0]*len(img[0]) for _ in range(len(img))]
+        ROWS, COLS= len(img), len(img[0])
+        for row in range(ROWS):
+            for col in range(COLS):
+                img_sum, img_count= 0, 0
+                for i in range(row-1, row+2):
+                    for j in range(col-1, col+2):
+                        if i>=0 and i<ROWS and j>=0 and j<COLS:
+                            img_sum+=img[i][j]
+                            img_count+=1
+                result_img[row][col]=(img_sum//img_count)
+        return result_img
