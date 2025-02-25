641861aa03393b87a926b294d1ba0f2780acbbc3
Dawit Zewdu <106137900+dawit2123@users.noreply.github.com>
2025-02-25 23:42:04 +0300

    Created 'Image Overlap' by A2SV Hub

---
+++ 25-Feb-2025/Image Overlap 282867.py
@@ -0,0 +1,20 @@
+# Problem: Image Overlap - https://leetcode.com/problems/image-overlap/description/
+
+from collections import Counter  
+from typing import List  
+
+class Solution:  
+    def largestOverlap(self, img1: List[List[int]], img2: List[List[int]]) -> int:  
+        size = len(img1)  # Use img1 instead of A  
+        overlap_count = Counter()  
+        
+        for i in range(size):  
+            for j in range(size):  
+                if img1[i][j] == 1:  # Use img1 instead of A  
+                    for h in range(size):  
+                        for k in range(size):  
+                            if img2[h][k] == 1:  # Use img2 instead of B  
+                                overlap_count[(i - h, j - k)] += 1  
+        
+        # No colon at the end  
+        return max(overlap_count.values()) if overlap_count else 0
