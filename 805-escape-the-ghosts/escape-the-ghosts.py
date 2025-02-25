8b60a49915f7b0fe9c0592d2a45bcee164712d8b
Dawit Zewdu <106137900+dawit2123@users.noreply.github.com>
2025-02-26 00:14:48 +0300

    Time: 0 ms (100.00%) | Memory: 17.9 MB (21.79%) - LeetSync

---
+++ 805-escape-the-ghosts/escape-the-ghosts.py
@@ -0,0 +1,6 @@
+class Solution:
+    def escapeGhosts(self, ghosts: List[List[int]], target: List[int]) -> bool:
+        target_x, target_y= target
+        return all(
+            (abs(target_x-ghost_x)+abs(target_y-ghost_y))>(abs(target_x)+abs(target_y)) for ghost_x, ghost_y in ghosts
+            )
