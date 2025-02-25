399659b8de57aef2b3ca930631c595904a08af6c
Dawit Zewdu <106137900+dawit2123@users.noreply.github.com>
2025-02-25 22:42:18 +0300

    Created 'Fox And Snake' by A2SV Hub

---
+++ 25-Feb-2025/Fox And Snake 282785.py
@@ -0,0 +1,14 @@
+# Problem: Fox And Snake - https://codeforces.com/problemset/problem/510/A
+
+row, column= list(map(int, input().split()))
+is_last=True
+for i in range(row):
+    if i%2==0:
+        print("#"*column)
+    else:
+        if is_last:
+            print("."*(column-1)+"#")
+            is_last=False
+        else:
+            print("#"+"."*(column-1))
+            is_last=True
