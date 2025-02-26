365d843a47b08d9b91997c851c05d5e0e7496fe5
Dawit Zewdu <106137900+dawit2123@users.noreply.github.com>
2025-02-26 10:03:43 +0300

    Created 'Team' by A2SV Hub

---
+++ 26-Feb-2025/Team 283034.py
@@ -0,0 +1,11 @@
+# Problem: Team - https://codeforces.com/contest/231/problem/A
+
+from collections import Counter
+n= int(input())
+number_of_problems_solved=0
+for _ in range(n):
+    solved_one= input()
+    cnt= Counter(solved_one.split(" "))
+    if cnt["1"]>=2:
+        number_of_problems_solved+=1
+print(number_of_problems_solved)
