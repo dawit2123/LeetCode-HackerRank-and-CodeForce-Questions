e263750cdd7f34e23ba0d355e31f53cd10a0ac83
Dawit Zewdu <106137900+dawit2123@users.noreply.github.com>
2025-02-26 10:11:11 +0300

    Created 'Way Too Long Words' by A2SV Hub

---
+++ 26-Feb-2025/Way Too Long Words 283040.py
@@ -0,0 +1,10 @@
+# Problem: Way Too Long Words - https://codeforces.com/problemset/problem/71/A
+
+n= int(input())
+for _ in range(n):
+    word= input()
+    length_of_word= len(word)
+    if length_of_word>10:
+        print(word[0]+str(length_of_word-2)+word[length_of_word-1])
+    else:
+        print(word)
