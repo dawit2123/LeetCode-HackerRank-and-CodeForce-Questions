43363e3d2c0dd5ebdbc3f5b19aa8d81ad8593207
Dawit Zewdu <106137900+dawit2123@users.noreply.github.com>
2025-02-26 22:33:31 +0300

    Created 'Majority Element' by A2SV Hub

---
+++ 26-Feb-2025/Majority Element 285454.py
@@ -0,0 +1,13 @@
+# Problem: Majority Element - https://leetcode.com/problems/majority-element/description/
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        n=2
+        nums_count=Counter(nums)
+        temp=float('-inf')
+        res=0
+        for num in nums_count:
+            if nums_count[num]>=n//2 and nums_count[num]>temp:
+                temp=nums_count[num]
+                res=num
+        return res
