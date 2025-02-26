b3616b4c1e5a6f690cf62fcf79f87ab2e525555a
Dawit Zewdu <106137900+dawit2123@users.noreply.github.com>
2025-02-26 11:06:24 +0300

    Created 'Presents' by A2SV Hub

---
+++ 26-Feb-2025/Presents 283094.py
@@ -0,0 +1,8 @@
+# Problem: Presents - https://codeforces.com/problemset/problem/136/A
+
+n= int(input())
+gifts= list(map(int, input().split()))
+result=[0]*n
+for i in range(n):
+    result[gifts[i]-1]=i+1
+print(" ".join(map(str, result)))
