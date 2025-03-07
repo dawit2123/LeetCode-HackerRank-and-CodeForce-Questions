cc2a7ed263f3c975498d85233b7574d3f98eedaa
Dawit Zewdu <106137900+dawit2123@users.noreply.github.com>
2025-03-07 20:45:10 +0300

    Time: 0 ms (100.00%) | Memory: 18.2 MB (20.08%) - LeetSync

---
+++ 1217-relative-sort-array/relative-sort-array.py
@@ -0,0 +1,11 @@
+class Solution:
+    def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
+        hash_map= Counter(arr1)
+        result=[]
+        arr2_set= set(arr2)
+        for num in arr2:
+            result.extend([num]*hash_map[num])
+        for key in sorted(hash_map.keys()):
+            if key not in arr2_set:
+                result.extend([key]*hash_map[key])
+        return result
