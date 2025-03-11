aab72f1f89de5259359ff09fb0de61f8264ecb23
Dawit Zewdu <106137900+dawit2123@users.noreply.github.com>
2025-03-11 17:33:39 +0300

    Time: 27 ms (87.05%) | Memory: 23 MB (6.47%) - LeetSync

---
+++ 436-find-right-interval/find-right-interval.py
@@ -0,0 +1,12 @@
+from bisect import bisect_left
+class Solution:
+    def findRightInterval(self, intervals: List[List[int]]) -> List[int]:
+        ans=[-1]*len(intervals)
+        for index, interval in enumerate(intervals):
+            interval.append(index)
+        intervals.sort()
+        for _, end, index in intervals:
+            right_index= bisect_left(intervals, [end])
+            if right_index<len(intervals):
+                ans[index]= intervals[right_index][2]
+        return ans
