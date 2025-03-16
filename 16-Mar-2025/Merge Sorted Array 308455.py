b9792d62b5ff45af32a30e14be5c19fad29f91fd
Dawit Zewdu <106137900+dawit2123@users.noreply.github.com>
2025-03-16 20:50:22 +0300

    Created 'Merge Sorted Array' by A2SV Hub

---
+++ 16-Mar-2025/Merge Sorted Array 308455.py
@@ -0,0 +1,22 @@
+# Problem: Merge Sorted Array - https://leetcode.com/problems/merge-sorted-array/
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        last_num1=m-1
+        last_num2=n-1
+        position=len(nums1)-1
+        while last_num2>=0 and last_num1>=0:
+            if nums2[last_num2] > nums1[last_num1]:
+                nums1[position]=nums2[last_num2]
+                last_num2-=1
+            else:
+                nums1[position]=nums1[last_num1]
+                last_num1-=1
+            position-=1
+        while last_num2>=0:
+            nums1[position]=nums2[last_num2]
+            position-=1
+            last_num2-=1
