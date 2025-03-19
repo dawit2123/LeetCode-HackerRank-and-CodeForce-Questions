419b399d4fcf3e4a0c2b0673f2430b60e850c98e
Dawit Zewdu <106137900+dawit2123@users.noreply.github.com>
2025-03-19 16:42:04 +0300

    Created 'Longest Repeating Character Replacement' by A2SV Hub

---
+++ 19-Mar-2025/Longest Repeating Character Replacement 312436.py
@@ -0,0 +1,14 @@
+# Problem: Longest Repeating Character Replacement - https://leetcode.com/problems/longest-repeating-character-replacement/
+
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        count_freq=defaultdict(int)
+        res=0
+        l=0
+        for r in range(len(s)):
+            count_freq[s[r]]+=1
+            while (sum(count_freq.values())-max(count_freq.values()))>k:
+                count_freq[s[l]]-=1
+                l+=1
+            res=max(res, r-l+1)
+        return res
