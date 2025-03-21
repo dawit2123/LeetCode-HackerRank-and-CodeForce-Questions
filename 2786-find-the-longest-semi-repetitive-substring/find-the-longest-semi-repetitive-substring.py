640c5d528a8f25b497623919c51dcf615449ce59
Dawit Zewdu <106137900+dawit2123@users.noreply.github.com>
2025-03-21 16:00:31 +0300

    Time: 15 ms (61.16%) | Memory: 18 MB (16.74%) - LeetSync

---
+++ 2786-find-the-longest-semi-repetitive-substring/find-the-longest-semi-repetitive-substring.py
@@ -0,0 +1,14 @@
+class Solution:
+    def longestSemiRepetitiveSubstring(self, s: str) -> int:
+        if len(s)<=1:
+            return len(s)
+        max_count=0
+        previous_pair=None
+        left=0
+        for right in range(1, len(s)):
+            if s[right]==s[right-1]:
+                if previous_pair:
+                    left= previous_pair
+                previous_pair=right
+            max_count= max(max_count, right-left+1)
+        return max_count
