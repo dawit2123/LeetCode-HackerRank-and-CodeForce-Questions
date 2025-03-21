978bd66974b76e2c13c52322f79ac395141953fb
Dawit Zewdu <106137900+dawit2123@users.noreply.github.com>
2025-03-21 22:58:44 +0300

    Created 'Find All Possible Recipes from Given Supplies' by A2SV Hub

---
+++ 21-Mar-2025/Find All Possible Recipes from Given Supplies 315282.py
@@ -0,0 +1,19 @@
+# Problem: Find All Possible Recipes from Given Supplies - https://leetcode.com/problems/find-all-possible-recipes-from-given-supplies/
+
+class Solution:
+    def findAllRecipes(self, recipes: List[str], ingredients: List[List[str]], supplies: List[str]) -> List[str]:
+        can_cook= {s:True for s in supplies}
+        recipes_map={r:i for i, r in enumerate(recipes)}
+
+        def dfs(r):
+            if r in can_cook:
+                return can_cook[r]
+            if r not in recipes_map:
+                return False
+            can_cook[r]=False
+            for neighbour in ingredients[recipes_map[r]]:
+                if not dfs(neighbour):
+                    return False
+            can_cook[r]=True
+            return can_cook[r]
+        return [recipe for recipe in recipes if dfs(recipe)]  
