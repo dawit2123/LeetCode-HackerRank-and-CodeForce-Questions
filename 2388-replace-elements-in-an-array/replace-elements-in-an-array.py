16bd2b38220248a83039eb0f435a1cb912a46757
Dawit Zewdu <106137900+dawit2123@users.noreply.github.com>
2025-03-30 23:48:53 +0300

    Time: 47 ms (97.21%) | Memory: 70.2 MB (40.86%) - LeetSync

---
+++ 2388-replace-elements-in-an-array/replace-elements-in-an-array.py
@@ -0,0 +1,8 @@
+class Solution:
+    def arrayChange(self, nums: List[int], operations: List[List[int]]) -> List[int]:
+        key_map= {val: key for key, val in enumerate(nums)}
+        for operation in operations:
+            index= key_map[operation[0]]
+            nums[index]= operation[1]
+            key_map[operation[1]]=index
+        return nums
