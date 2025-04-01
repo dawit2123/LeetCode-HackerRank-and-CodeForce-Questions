31e7d681a3218f0d031945b35ec359eb44358134
Dawit Zewdu <106137900+dawit2123@users.noreply.github.com>
2025-04-01 16:25:30 +0300

    Created 'Design Underground System' by A2SV Hub

---
+++ 01-Apr-2025/Design Underground System 323210.py
@@ -0,0 +1,18 @@
+# Problem: Design Underground System - https://leetcode.com/problems/design-underground-system/
+
+class UndergroundSystem:
+
+    def __init__(self):
+        self.user = defaultdict(tuple)
+        self.stat = defaultdict(list)
+
+    def checkIn(self, id: int, stationName: str, t: int) -> None:
+        self.user[id] = (stationName,t)
+
+    def checkOut(self, id: int, stationName: str, t: int) -> None:
+        chkin = self.user[id]
+        self.stat['_'.join([chkin[0],stationName])].append(t-chkin[1])
+
+    def getAverageTime(self, startStation: str, endStation: str) -> float:
+        arr = self.stat['_'.join([startStation,endStation])]
+        return sum(arr)/len(arr)
