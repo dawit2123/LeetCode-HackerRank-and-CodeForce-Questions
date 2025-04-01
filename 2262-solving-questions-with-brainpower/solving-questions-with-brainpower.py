865c546edac3b21a434fe424c581e83894891102
Dawit Zewdu <106137900+dawit2123@users.noreply.github.com>
2025-04-01 16:09:36 +0300

    Time: 152 ms (40.24%) | Memory: 66 MB (38.94%) - LeetSync

---
+++ 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.py
@@ -0,0 +1,13 @@
+class Solution:
+    def mostPoints(self, questions: List[List[int]]) -> int:
+        cache=[0]*len(questions)
+        def backtrack(i):
+            if i>=len(questions):
+                return 0
+            point, power= questions[i]
+            if cache[i]!=0:
+                return cache[i]
+            else:
+                cache[i] = max(backtrack(i+1), point + backtrack(i+1+power))
+                return cache[i]
+        return backtrack(0)
