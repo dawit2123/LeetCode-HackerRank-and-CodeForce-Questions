a9788a76edf82b7741e810019f56643157a54a7b
Dawit Zewdu <106137900+dawit2123@users.noreply.github.com>
2025-04-02 20:54:33 +0300

    Created '4Sum II' by A2SV Hub

---
+++ 02-Apr-2025/4Sum II 324210.py
@@ -0,0 +1,13 @@
+# Problem: 4Sum II - https://leetcode.com/problems/4sum-ii/
+
+class Solution:
+    def fourSumCount(self, nums1: List[int], nums2: List[int], nums3: List[int], nums4: List[int]) -> int:
+        result=0
+        sum_map=defaultdict(int)
+        for num1 in nums1:
+            for num2 in nums2:
+                sum_map[num1+num2]+=1
+        for num3 in nums3:
+            for num4 in nums4:
+                result+=sum_map[-(num3+num4)]
+        return result
