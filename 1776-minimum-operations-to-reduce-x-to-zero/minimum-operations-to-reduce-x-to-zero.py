568ca60e07e3b8b18fbe0e5d682a05d8e8bffca9
Dawit Zewdu <106137900+dawit2123@users.noreply.github.com>
2025-04-11 22:47:21 +0300

    Time: 103 ms (48.72%) | Memory: 29.3 MB (40.82%) - LeetSync

---
+++ 1776-minimum-operations-to-reduce-x-to-zero/minimum-operations-to-reduce-x-to-zero.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minOperations(self, nums: List[int], x: int) -> int:
+        answer=float("inf")
+        target_sum= sum(nums)-x
+        left_index=current_sum=0
+        for right_index, val in enumerate(nums):
+            current_sum+=val
+            while left_index<=right_index and current_sum>target_sum:
+                current_sum-=nums[left_index]
+                left_index+=1
+            if current_sum==target_sum:
+                answer= min(answer, len(nums)-(right_index-left_index+1))
+        return -1 if answer==float("inf") else answer
+                
