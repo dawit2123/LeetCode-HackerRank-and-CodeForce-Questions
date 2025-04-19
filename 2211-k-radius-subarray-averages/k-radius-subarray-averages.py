5bc2ecccabe12d7ae7b372bb7c9364e624b3c553
Dawit Zewdu <106137900+dawit2123@users.noreply.github.com>
2025-04-19 20:02:51 +0300

    Time: 71 ms (83.16%) | Memory: 31.9 MB (95.78%) - LeetSync

---
+++ 2211-k-radius-subarray-averages/k-radius-subarray-averages.py
@@ -0,0 +1,12 @@
+class Solution(object):  
+    def getAverages(self, values, radius):  
+        length = len(values)  
+        averages = [-1] * length  
+        if length < radius * 2 + 1:  
+            return averages  
+        window_sum = sum(values[:2*radius+1])  
+        averages[radius] = window_sum // (2*radius + 1)  
+        for idx in range(radius + 1, length - radius):  
+            window_sum += (values[idx + radius] - values[idx - radius - 1])  
+            averages[idx] = window_sum // (2*radius + 1)  
+        return averages  
