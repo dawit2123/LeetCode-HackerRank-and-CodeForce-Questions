bb904590b1005ae3130a8fdfac358dd9bd05c970
Dawit Zewdu <106137900+dawit2123@users.noreply.github.com>
2025-04-27 21:47:50 +0300

    Created 'Next Greater Element' by A2SV Hub

---
+++ 27-Apr-2025/Next Greater Element 343793.py
@@ -0,0 +1,17 @@
+# Problem: Next Greater Element - https://leetcode.com/problems/next-greater-element-i/
+
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        answer=[-1]*len(nums1)
+        num_to_index= {n:i for i, n in enumerate(nums1)}
+        stack=[]
+        for num in nums2:
+            while stack and num > stack[-1]:
+                val=stack.pop()
+                index=num_to_index[val]
+                answer[index]=num
+            if num in num_to_index:
+                stack.append(num)
+        return answer
+
+            
