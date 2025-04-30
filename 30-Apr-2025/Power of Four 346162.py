404ec38c670260b3246caf11bac34e8fc9add85c
Dawit Zewdu <106137900+dawit2123@users.noreply.github.com>
2025-05-01 00:02:33 +0300

    Created 'Power of Four' by A2SV Hub

---
+++ 30-Apr-2025/Power of Four 346162.py
@@ -0,0 +1,16 @@
+# Problem: Power of Four - https://leetcode.com/problems/power-of-four/
+
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+        if n <= 0:
+            return False
+        if n == 1:
+            return True
+
+        while n > 1:
+            # use the modulus operator to check if it's divisible by 4 using the logarithmic approach
+            if n % 4 != 0:
+                return False
+            n //= 4
+
+        return True        
