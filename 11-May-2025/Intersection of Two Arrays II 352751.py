ad56cfafd6f21eeb00358a50586e235e5384cdee
Dawit Zewdu <106137900+dawit2123@users.noreply.github.com>
2025-05-11 21:00:23 +0300

    Created 'Intersection of Two Arrays II' by A2SV Hub

---
+++ 11-May-2025/Intersection of Two Arrays II 352751.py
@@ -0,0 +1,12 @@
+# Problem: Intersection of Two Arrays II - https://leetcode.com/problems/intersection-of-two-arrays-ii/
+
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        res=[]
+        freq=Counter(nums1)
+        for num in nums2:
+            if freq[num]>0:
+                res.append(num)
+                freq[num]-=1
+        return res
+            
