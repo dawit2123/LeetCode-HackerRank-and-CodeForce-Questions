67350dac4ee653b98cb33d30f6647be8426aea5e
Dawit Zewdu <106137900+dawit2123@users.noreply.github.com>
2025-05-29 16:11:02 -0400

    Created 'Path Sum' by A2SV Hub

---
+++ 29-May-2025/Path Sum 361084.py
@@ -0,0 +1,18 @@
+# Problem: Path Sum - https://leetcode.com/problems/path-sum/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        def dfs(root, cur_sum):
+            if not root:
+                return
+            cur_sum+=root.val
+            if cur_sum==targetSum and not root.left and not root.right:
+                return True
+            return dfs(root.left, cur_sum) or dfs(root.right, cur_sum)
+        return True if dfs(root, 0)==True else False
