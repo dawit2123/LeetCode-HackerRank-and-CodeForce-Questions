abbdc1f484915d1dfb5350e429ffaae5923d00bb
Dawit Zewdu <106137900+dawit2123@users.noreply.github.com>
2025-06-02 12:46:33 -0400

    Created 'Generate Parentheses' by A2SV Hub

---
+++ 02-Jun-2025/Generate Parentheses 362834.py
@@ -0,0 +1,14 @@
+# Problem: Generate Parentheses - https://leetcode.com/problems/generate-parentheses/description/
+
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        res=[]
+        def dfs(open, close, cur):
+            if  open>n or close>n:
+                return 
+            if open==close==n:
+                res.append(cur)
+            dfs(open+1, close, cur+"(")
+            if open!=close: dfs(open, close+1, cur+")")
+        dfs(1,0, "(")
+        return res         
