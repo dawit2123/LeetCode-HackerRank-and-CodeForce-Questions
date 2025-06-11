fff68111e661cce031db2207ee7e17774b88c3b2
Dawit Zewdu <106137900+dawit2123@users.noreply.github.com>
2025-06-11 23:39:07 +0300

    Created 'Fair Distribution of Cookies' by A2SV Hub

---
+++ 11-Jun-2025/Fair Distribution of Cookies 365753.py
@@ -0,0 +1,19 @@
+# Problem: Fair Distribution of Cookies - https://leetcode.com/problems/fair-distribution-of-cookies/
+
+class Solution:
+    def distributeCookies(self, cookies: List[int], k: int) -> int:
+        l = [0]*k 
+        self.s = float('inf')
+        def ser(l,i):
+            if i>=len(cookies):
+                self.s = min(self.s,max(l))
+                return 
+            if max(l)>=self.s:
+                return 
+            for j in range(k):
+                l[j]+=cookies[i]
+                ser(l,i+1)
+                l[j]-=cookies[i]
+        
+        ser(l,0)
+        return self.s
