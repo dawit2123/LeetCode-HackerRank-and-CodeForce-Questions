58b94839fe5077392dd1358a7f1d76487d725a19
Dawit Zewdu <106137900+dawit2123@users.noreply.github.com>
2025-07-25 09:56:25 +0300

    Time: 2 ms (33.96%) | Memory: 17.6 MB (85.25%) - LeetSync

--- 3788-maximum-unique-subarray-sum-after-deletion/maximum-unique-subarray-sum-after-deletion.py
+++ 3788-maximum-unique-subarray-sum-after-deletion/maximum-unique-subarray-sum-after-deletion.py
@@ -1,18 +1,11 @@
 class Solution:
     def maxSum(self, nums: List[int]) -> int:
-        max_sum=float('-inf')
-        nums.sort()
-        visited=set()
-        if nums[-1]<0:
-            return nums[-1]
+        if max(nums)<0 :
+            return max(nums)
         else:
-            temp_sum=0
-            for i in range(len(nums)-1, -1, -1):
-                if nums[i] not in visited:
-                    temp_sum+=nums[i]
-                    if temp_sum>0:
-                        max_sum= max(max_sum, temp_sum)
-                        visited.add(nums[i])
-                    else:
-                        temp_sum=0
-        return max_sum if max_sum!=float('-inf') else 0
+            result=0
+            nums=set(nums)
+            for i in nums:
+                if i>=0:
+                    result+=i
+            return result
