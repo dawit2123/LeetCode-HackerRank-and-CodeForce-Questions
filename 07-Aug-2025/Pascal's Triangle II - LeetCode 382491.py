540288bf2fe711df9b20402905e731ffec7782cf
Dawit Zewdu <106137900+dawit2123@users.noreply.github.com>
2025-08-07 05:05:57 -0400

    Created 'Pascal's Triangle II - LeetCode' by A2SV Hub

---
+++ 07-Aug-2025/Pascal's Triangle II - LeetCode 382491.py
@@ -0,0 +1,11 @@
+# Problem: Pascal's Triangle II - LeetCode - https://leetcode.com/problems/pascals-triangle-ii/
+
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        rows = [1]
+
+        for i in range(1, rowIndex + 1):
+            next_element = rows[i - 1] * (rowIndex - i + 1) // i
+            rows.append(next_element)
+
+        return rows
