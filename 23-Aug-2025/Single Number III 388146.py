d513f3a4db9363558016b37c022327da7caa149e
Dawit Zewdu <106137900+dawit2123@users.noreply.github.com>
2025-08-23 15:45:11 -0400

    Created 'Single Number III' by A2SV Hub

---
+++ 23-Aug-2025/Single Number III 388146.py
@@ -0,0 +1,12 @@
+# Problem: Single Number III - https://leetcode.com/problems/single-number-iii/
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> List[int]:
+        xor_set_numbers= reduce(xor, nums)
+        pviot_bit= xor_set_numbers & -(xor_set_numbers)
+        unique_number1= 0
+        for num in nums:
+            if num&pviot_bit:
+                unique_number1^=num
+        unique_number2= xor_set_numbers^unique_number1
+        return [unique_number1, unique_number2]
