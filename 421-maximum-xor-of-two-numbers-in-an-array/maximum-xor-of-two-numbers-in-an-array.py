d63a6b8436c2440183d0bdde5b629c58414ee721
Dawit Zewdu <106137900+dawit2123@users.noreply.github.com>
2025-08-25 05:26:42 -0400

    Time: 1142 ms (80.64%) | Memory: 66.9 MB (69.36%) - LeetSync

---
+++ 421-maximum-xor-of-two-numbers-in-an-array/maximum-xor-of-two-numbers-in-an-array.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findMaximumXOR(self, nums: List[int]) -> int:
+        # initializing the ans variable
+        ans=0
+        # build a prefix for a numbers from 32 to 1
+        for i in range(31, -1, -1):
+            ans<<=1
+            prefix={num>>i for num in nums}
+            target=ans^1
+            can_set_bit= any((target^p) in prefix for p in prefix)
+            ans+=1 if can_set_bit else 0
+        return ans
+        
