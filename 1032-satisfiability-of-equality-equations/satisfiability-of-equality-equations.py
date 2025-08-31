84d220c594f36c457a401a91dabccff603f69070
Dawit Zewdu <106137900+dawit2123@users.noreply.github.com>
2025-08-31 15:58:39 -0400

    Time: 3 ms (59.94%) | Memory: 17.9 MB (55.87%) - LeetSync

---
+++ 1032-satisfiability-of-equality-equations/satisfiability-of-equality-equations.py
@@ -0,0 +1,27 @@
+from typing import List
+
+class Solution:
+    def equationsPossible(self, equations: List[str]) -> bool:
+        def find(x: int) -> int:
+            if parent[x] != x:
+                parent[x] = find(parent[x])
+            return parent[x]
+
+        parent = list(range(26))
+
+        for equation in equations:
+            var1_index = ord(equation[0]) - ord('a')
+            var2_index = ord(equation[-1]) - ord('a')
+            if equation[1] == '=':
+                root1 = find(var1_index)
+                root2 = find(var2_index)
+                parent[root1] = root2
+
+        for equation in equations:
+            var1_index = ord(equation[0]) - ord('a')
+            var2_index = ord(equation[-1]) - ord('a')
+            if equation[1] == '!':
+                if find(var1_index) == find(var2_index):
+                    return False
+
+        return True
