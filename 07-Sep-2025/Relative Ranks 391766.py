1b4c52fe7c25061425d98b303999146cb883cef9
Dawit Munie <106137900+dawit2123@users.noreply.github.com>
2025-09-07 07:25:05 -0400

    Created 'Relative Ranks' by A2SV Hub

---
+++ 07-Sep-2025/Relative Ranks 391766.py
@@ -0,0 +1,21 @@
+# Problem: Relative Ranks - https://leetcode.com/problems/relative-ranks/description/
+
+class Solution:
+    def findRelativeRanks(self, score: List[int]) -> List[str]:
+        res=[0]*len(score)
+        place=1
+        heap=[]
+        for index, val in enumerate(score):
+            heapq.heappush(heap, [-1*val, index])
+        while heap:
+            max_score, max_index= heapq.heappop(heap)
+            if place==1:
+                res[max_index]="Gold Medal"
+            elif place==2:
+                res[max_index]="Silver Medal"
+            elif place==3:
+                res[max_index]="Bronze Medal"
+            else:
+                res[max_index]=str(place)
+            place+=1
+        return res
