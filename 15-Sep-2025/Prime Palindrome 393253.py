37ec21b6c50823b8dc843c61a2f7d57d5bb87d7f
Dawit Munie <106137900+dawit2123@users.noreply.github.com>
2025-09-15 04:24:51 -0400

    Created 'Prime Palindrome' by A2SV Hub

---
+++ 15-Sep-2025/Prime Palindrome 393253.py
@@ -0,0 +1,27 @@
+# Problem: Prime Palindrome - https://leetcode.com/problems/prime-palindrome/description/?envType=problem-list-v2&envId=number-theory
+
+class Solution:
+    def primePalindrome(self, n: int) -> int:
+        def is_prime(x):
+            if x < 2:
+                return False
+            divisor = 2
+            while divisor * divisor <= x:
+                if x % divisor == 0:
+                    return False
+                divisor += 1
+            return True
+
+        def reverse(x):
+            result = 0
+            while x:
+                result = result * 10 + x % 10  
+                x //= 10  
+            return result
+
+        while True:
+            if reverse(n) == n and is_prime(n):
+                return n
+            if 10**7 < n < 10**8:
+                n = 10**8
+            n += 1
