52caad943f67347af308c916ff8d0ad8f8a88193
Dawit Munie <106137900+dawit2123@users.noreply.github.com>
2025-09-25 05:25:41 -0400

    Created 'Design Add and Search Words Data Structure' by A2SV Hub

---
+++ 25-Sep-2025/Design Add and Search Words Data Structure 396150.py
@@ -0,0 +1,39 @@
+# Problem: Design Add and Search Words Data Structure - https://leetcode.com/problems/design-add-and-search-words-data-structure/
+
+class TrieNode:
+    def __init__(self):
+        self.children={}
+        self.is_end_of_word=False
+class WordDictionary:
+
+    def __init__(self):
+        self.root=TrieNode()
+
+    def addWord(self, word: str) -> None:
+        cur=self.root
+        for c in word:
+            if c not in cur.children:
+                cur.children[c]=TrieNode()
+            cur=cur.children[c]
+        cur.is_end_of_word=True
+    def search(self, word: str) -> bool:
+        def dfs(j, root):
+            cur=root
+            for i in range(j,len(word)):
+                if word[i]==".":
+                    for val in cur.children.values():
+                        if dfs(i+1, val):
+                            return True
+                    return False
+                else:
+                    if word[i] not in cur.children:
+                        return False
+                    cur=cur.children[word[i]]
+            return cur.is_end_of_word
+        return dfs(0, self.root)
+
+
+# Your WordDictionary object will be instantiated and called as such:
+# obj = WordDictionary()
+# obj.addWord(word)
+# param_2 = obj.search(word)
