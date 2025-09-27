143c516dd329199c08341ac5d04c2f3bc06fa302
Dawit Munie <106137900+dawit2123@users.noreply.github.com>
2025-09-27 13:41:00 -0400

    Time: 0 ms (100.00%) | Memory: 17.9 MB (69.93%) - LeetSync

---
+++ 677-map-sum-pairs/map-sum-pairs.py
@@ -0,0 +1,34 @@
+from collections import deque
+
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.val = 0
+
+class MapSum:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, key: str, val: int) -> None:
+        node = self.root
+        for ch in key:
+            if ch not in node.children:
+                node.children[ch] = TrieNode()
+            node = node.children[ch]
+        node.val = val
+
+    def sum(self, prefix: str) -> int:
+        node = self.root
+        for ch in prefix:
+            if ch not in node.children:
+                return 0
+            node = node.children[ch]
+        ans = 0
+        q = deque([node])
+        while q:
+            node = q.popleft()
+            ans += node.val
+            for child in node.children.values():
+                q.append(child)
+        return ans
