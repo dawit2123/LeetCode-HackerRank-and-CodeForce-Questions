5a56a07e3374c426dc7e0f695f79eb8b1895e351
Dawit Munie <106137900+dawit2123@users.noreply.github.com>
2025-10-05 07:55:24 -0400

    Time: 0 ms (100.00%) | Memory: 17.8 MB (84.67%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,21 +1,10 @@
-class Solution(object):
-    def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        hashmap={")":"(","]":"[","}":"{"}
+class Solution:
+    def isValid(self, s: str) -> bool:
+        hash_map={")":"(", "}":"{", "]":"["}
         stack=[]
-        for i in s:
-            if(i in "({["):
-                stack.append(i);
+        for c in s:
+            if stack and c in hash_map and stack[-1]==hash_map[c]:
+                stack.pop()
             else:
-                if stack and stack[-1]==hashmap[i]:
-                    stack.pop()
-                else:
-                    return False
-        if not stack:
-            return True
-        else:
-            return False
-               
+                stack.append(c)
+        return len(stack)==0
