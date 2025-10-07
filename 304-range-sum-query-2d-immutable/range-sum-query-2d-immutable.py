2f321668775c829972ffa2e56bbf28127838bd0c
Dawit Munie <106137900+dawit2123@users.noreply.github.com>
2025-10-07 06:41:00 -0400

    Time: 116 ms (53.71%) | Memory: 30.8 MB (80.06%) - LeetSync

--- 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.py
+++ 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.py
@@ -1,22 +1,24 @@
 class NumMatrix:
 
     def __init__(self, matrix: List[List[int]]):
-        ROWS,COLS= len(matrix), len(matrix[0])
-        self.sum_matrix= [[0]*(COLS+1) for r in range(ROWS+1)]
-        for R in range(ROWS):
-            prefix=0
-            for C in range(COLS):
-                prefix+=matrix[R][C]
-                above=self.sum_matrix[R][C+1]
-                self.sum_matrix[R+1][C+1]=prefix+above
+        ROWS, COLS= len(matrix), len(matrix[0])
+        self.prefixSum=[[0]*(COLS+1) for _ in range(ROWS+1)]
+        for r in range(ROWS):
+            for c in range(COLS):
+                self.prefixSum[r+1][c+1]=(
+                    self.prefixSum[r][c+1]+
+                    self.prefixSum[r+1][c]-
+                    self.prefixSum[r][c]+
+                    matrix[r][c]
+                )
 
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
-        row1, row2, col1, col2= row1+1, row2+1, col1+1, col2+1
-        upper= self.sum_matrix[row1-1][col2]
-        left=self.sum_matrix[row2][col1-1]
-        top_left=self.sum_matrix[row1-1][col1-1]
-        bottom_right=self.sum_matrix[row2][col2]
-        return bottom_right-upper-left+top_left
+        return (
+            self.prefixSum[row2+1][col2+1]
+            - self.prefixSum[row1][col2+1]
+            - self.prefixSum[row2+1][col1]
+            + self.prefixSum[row1][col1]
+        )
 
 # Your NumMatrix object will be instantiated and called as such:
 # obj = NumMatrix(matrix)
