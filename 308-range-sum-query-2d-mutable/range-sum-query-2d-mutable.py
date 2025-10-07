ab0c5d2cfdbe11ac0b22fcb66920b64a77706b67
Dawit Munie <106137900+dawit2123@users.noreply.github.com>
2025-10-07 07:36:23 -0400

    Time: 800 ms (43.87%) | Memory: 27.4 MB (74.19%) - LeetSync

--- 308-range-sum-query-2d-mutable/range-sum-query-2d-mutable.py
+++ 308-range-sum-query-2d-mutable/range-sum-query-2d-mutable.py
@@ -1,45 +1,39 @@
-class BinaryIndexedTree:
+class BIT:
     def __init__(self, n):
-        self.n = n
-        self.c = [0] * (n + 1)
-
+        self.n=n
+        self.c=[0]*(n+1)
     @staticmethod
     def lowbit(x):
-        return x & -x
-
+        return x&-x
     def update(self, x, delta):
-        while x <= self.n:
-            self.c[x] += delta
-            x += BinaryIndexedTree.lowbit(x)
-
+        while x<=self.n:
+            self.c[x]+=delta
+            x+=BIT.lowbit(x)
     def query(self, x):
-        s = 0
-        while x > 0:
-            s += self.c[x]
-            x -= BinaryIndexedTree.lowbit(x)
+        s=0
+        while x>0:
+            s+=self.c[x]
+            x-=BIT.lowbit(x)
         return s
-
-
+    
 class NumMatrix:
+
     def __init__(self, matrix: List[List[int]]):
-        self.trees = []
-        n = len(matrix[0])
+        self.trees=[]
+        n=len(matrix[0])
         for row in matrix:
-            tree = BinaryIndexedTree(n)
+            tree= BIT(n)
             for j, v in enumerate(row):
-                tree.update(j + 1, v)
+                tree.update(j+1, v)
             self.trees.append(tree)
 
     def update(self, row: int, col: int, val: int) -> None:
-        tree = self.trees[row]
-        prev = tree.query(col + 1) - tree.query(col)
-        tree.update(col + 1, val - prev)
+        tree=self.trees[row]
+        prev=tree.query(col+1)-tree.query(col)
+        tree.update(col+1,val-prev)
 
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
-        return sum(
-            tree.query(col2 + 1) - tree.query(col1)
-            for tree in self.trees[row1 : row2 + 1]
-        )
+        return sum(tree.query(col2+1)-tree.query(col1) for tree in self.trees[row1:row2+1])
 
 
 # Your NumMatrix object will be instantiated and called as such:
