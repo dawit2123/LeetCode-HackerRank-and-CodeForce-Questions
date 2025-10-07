3375efab4494f5bf821c158af73f45428dafb50d
Dawit Munie <106137900+dawit2123@users.noreply.github.com>
2025-10-07 02:16:58 -0400

    Time: 3 ms (25.99%) | Memory: 17.7 MB (95.80%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -1,17 +1,9 @@
-class Solution(object):
-    def longestCommonPrefix(self, strs):
-        """
-        :type strs: List[str]
-        :rtype: str
-        """
-        ans=""
-        sortedList= sorted(strs)
-        sortedListFirst=sortedList[0]
-        sortedListLast=sortedList[-1]
-        for i in range(min(len(sortedListFirst), len(sortedListLast))):
-            if(sortedListFirst[i]!= sortedListLast[i]):
-                return ans
-            else:
-                ans+= sortedListFirst[i]
-        return ans
-        
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        res=""
+        for i in range(len(strs[0])):
+            for s in strs:
+                if i==len(s) or strs[0][i]!=s[i]:
+                    return res
+            res+=strs[0][i]
+        return res
