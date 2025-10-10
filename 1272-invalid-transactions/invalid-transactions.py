4f7f6f1361a9181c429f0d0195a52a2e8c2d979e
Dawit Munie <106137900+dawit2123@users.noreply.github.com>
2025-10-10 04:28:13 -0400

    Time: 23 ms (56.67%) | Memory: 18.3 MB (82.32%) - LeetSync

--- 1272-invalid-transactions/invalid-transactions.py
+++ 1272-invalid-transactions/invalid-transactions.py
@@ -1,16 +1,16 @@
 class Solution:
     def invalidTransactions(self, transactions: List[str]) -> List[str]:
-        invalid_sets=set()
-        transactions_per_name=defaultdict(list)
-        for index, transaction in enumerate(transactions):
+        invalid_transaction_indexes= set()
+        transactions_map=defaultdict(list)
+        for i, transaction in enumerate(transactions):
             name, time, amount, city= transaction.split(",")
-            time=int(time)
-            amount=int(amount)
-            transactions_per_name[name].append([index, time, city])
+            time= int(time)
+            amount= int(amount)
             if amount>1000:
-                invalid_sets.add(index)
-            for prev_index, prev_time, prev_city in transactions_per_name[name]:
+                invalid_transaction_indexes.add(i)
+            for prev_time, prev_city, index in transactions_map[name]:
                 if prev_city!=city and abs(prev_time-time)<=60:
-                    invalid_sets.add(index)
-                    invalid_sets.add(prev_index)
-        return [transactions[index] for index in invalid_sets]
+                    invalid_transaction_indexes.add(i)
+                    invalid_transaction_indexes.add(index)
+            transactions_map[name].append([time, city, i])
+        return [transactions[i] for i in invalid_transaction_indexes]
