ddc503cf3d6b1b153cd499a67ac05cefa6f7689a
Dawit Munie <106137900+dawit2123@users.noreply.github.com>
2025-10-10 10:19:01 -0400

    Time: 108 ms (53.71%) | Memory: 20.7 MB (40.65%) - LeetSync

--- 676-implement-magic-dictionary/implement-magic-dictionary.py
+++ 676-implement-magic-dictionary/implement-magic-dictionary.py
@@ -1,48 +1,40 @@
-from typing import List
-
-class TrieNode:
+class Trie:
     def __init__(self):
-        self.children = {}
-        self.is_end_of_word = False
-
+        self.children={}
+        self.word=False
 class MagicDictionary:
 
     def __init__(self):
-        self.root = TrieNode()
+        self.root=Trie()
 
     def buildDict(self, dictionary: List[str]) -> None:
         for word in dictionary:
-            cur = self.root
-            for char in word:
-                if char not in cur.children:
-                    cur.children[char] = TrieNode()
-                cur = cur.children[char]
-            cur.is_end_of_word = True
+            cur=self.root
+            for c in word:
+                if c not in cur.children:
+                    cur.children[c]=Trie()
+                cur=cur.children[c]
+            cur.word=True
+        
 
     def search(self, searchWord: str) -> bool:
-        def dfs(node, index, changes_allowed):
-            if changes_allowed < 0:
+        def dfs(i, cur, max_replace):
+            if max_replace<0:
                 return False
-            
-            if index == len(searchWord):
-                return node.is_end_of_word and changes_allowed == 0
-
-            char_to_match = searchWord[index]
-
-            # Try to match the character directly
-            if char_to_match in node.children:
-                if dfs(node.children[char_to_match], index + 1, changes_allowed):
+            if i==(len(searchWord)):
+                return cur.word and max_replace==0
+            c=searchWord[i]   
+            if c in cur.children:
+                if dfs(i+1, cur.children[c], max_replace):
                     return True
-
-            # Try to change the current character
-            if changes_allowed > 0:
-                for char in node.children:
-                    if char != char_to_match:
-                        if dfs(node.children[char], index + 1, changes_allowed - 1):
+            if max_replace>0:
+                for char in cur.children:
+                    if char!=c:
+                        if dfs(i+1, cur.children[char], max_replace-1):
                             return True
             return False
+        return dfs(0, self.root, 1)
 
-        return dfs(self.root, 0, 1) # Start with 1 change allowed
 
 # Your MagicDictionary object will be instantiated and called as such:
 # obj = MagicDictionary()
