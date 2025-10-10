f827dccb14c4252cf1f042be11f09c6637d519e4
Dawit Munie <106137900+dawit2123@users.noreply.github.com>
2025-10-10 01:37:56 -0400

    Created 'Find Beautiful Indices in the Given Array I' by A2SV Hub

---
+++ 10-Oct-2025/Find Beautiful Indices in the Given Array I 401756.py
@@ -0,0 +1,19 @@
+# Problem: Find Beautiful Indices in the Given Array I - https://leetcode.com/problems/find-beautiful-indices-in-the-given-array-i/description/
+
+class Solution:
+    def beautifulIndices(self, s: str, a: str, b: str, k: int) -> List[int]:
+        ans = []
+        indices_a, indices_b = [], []        
+        for i in range(len(s) - len(a) + 1):
+            if s[i:i+len(a)] == a:
+                indices_a.append(i)        
+        for j in range(len(s) - len(b) + 1):
+            if s[j:j+len(b)] == b:
+                indices_b.append(j)        
+        for i in indices_a:
+            for j in indices_b:
+                if abs(i - j) <= k:
+                    ans.append(i)
+                    break        
+        ans.sort()        
+        return ans
