8ea625f55379643701cfb852b24a406ce812e6f2
Dawit Munie <106137900+dawit2123@users.noreply.github.com>
2025-10-10 11:34:15 -0400

    Time: 49 ms (36.96%) | Memory: 20.9 MB (11.66%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
@@ -1,28 +1,23 @@
 class Solution:
     def minRemoveToMakeValid(self, s: str) -> str:
         result=[]
-        cnt=0
-        for c in s:
-            if c=="(":
-                cnt+=1
-                result.append(c)
-            elif c==")" and cnt>0:
-                result.append(c)
-                cnt-=1
-            elif c!=")":
-                result.append(c)
-        # check for the reverse one
-        filtered=[]
-        back_cnt=0
-        for i in range(len(result)-1, -1, -1):
-            if result[i]==")":
-                filtered.append(result[i])
-                back_cnt+=1
-            elif result[i]=="(" and back_cnt>0:
-                filtered.append(result[i])
-                back_cnt-=1
-            elif result[i]!="(":
-                filtered.append(result[i])
-        filtered=filtered[::-1]
-        return "".join(filtered)
-
+        count=0
+        for i in range(len(s)):
+            if s[i]=="(":
+                result.append(s[i])
+                count+=1
+            elif s[i]==")" and count>0:
+                result.append(s[i])
+                count-=1
+            elif s[i]==")" and count<=0:
+                continue
+            else:
+                result.append(s[i])
+        temp=[]
+        # iterate from the backward and remove the ( slashes
+        for j in range(len(result)-1, -1, -1):
+            if result[j]=="(" and count>0:
+                count-=1
+            else:
+                temp.append(result[j])
+        return "".join(temp[::-1])
