a6560b6abec92ef9c67552d5832fb2a9dc52cc82
Dawit Munie <106137900+dawit2123@users.noreply.github.com>
2025-10-11 02:24:45 -0400

    Time: 20 ms (61.47%) | Memory: 19.5 MB (71.27%) - LeetSync

---
+++ 1436-get-watched-videos-by-your-friends/get-watched-videos-by-your-friends.py
@@ -0,0 +1,12 @@
+class Solution:
+    def watchedVideosByFriends(self, watchedVideos: List[List[str]],
+                 friends: List[List[int]], id: int, level: int) -> List[str]:
+
+        unseen = set(range(len(watchedVideos))) - (row:={id})
+
+        for _ in range(level):
+            unseen-= row
+            row = set(chain(*[friends[r] for r in row])) & unseen
+
+        ctr = Counter(chain(*[watchedVideos[r] for r in row]))
+        return sorted(ctr.keys(), key = lambda x: (ctr[x],x))     
