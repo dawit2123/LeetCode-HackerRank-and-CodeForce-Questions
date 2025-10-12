948d346220a717ca534988cc603c737e433b0511
Dawit Munie <106137900+dawit2123@users.noreply.github.com>
2025-10-12 16:31:19 -0400

    Time: 794 ms (43.21%) | Memory: 27.3 MB (72.84%) - LeetSync

--- 308-range-sum-query-2d-mutable/range-sum-query-2d-mutable.py
+++ 308-range-sum-query-2d-mutable/range-sum-query-2d-mutable.py
@@ -1,40 +1,42 @@
 class BIT:
     def __init__(self, n):
         self.n=n
-        self.c=[0]*(n+1)
+        self.values=[0]*(self.n+1)
     @staticmethod
-    def lowbit(x):
+    def lowerbit(x):
         return x&-x
+    # update
     def update(self, x, delta):
         while x<=self.n:
-            self.c[x]+=delta
-            x+=BIT.lowbit(x)
+            self.values[x]+=delta
+            x+=BIT.lowerbit(x)
+    # query
     def query(self, x):
         s=0
         while x>0:
-            s+=self.c[x]
-            x-=BIT.lowbit(x)
+            s+=(self.values[x])
+            x-=BIT.lowerbit(x)
         return s
-    
 class NumMatrix:
 
     def __init__(self, matrix: List[List[int]]):
         self.trees=[]
         n=len(matrix[0])
         for row in matrix:
-            tree= BIT(n)
-            for j, v in enumerate(row):
-                tree.update(j+1, v)
+            tree=BIT(n)
+            for j in range(n):
+                tree.update(j+1, row[j])
             self.trees.append(tree)
 
+
     def update(self, row: int, col: int, val: int) -> None:
-        tree=self.trees[row]
-        prev=tree.query(col+1)-tree.query(col)
-        tree.update(col+1,val-prev)
+        tree= self.trees[row]
+        prev= tree.query(col+1)-tree.query(col)
+        tree.update(col+1, val-prev)
 
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
-        return sum(tree.query(col2+1)-tree.query(col1) for tree in self.trees[row1:row2+1])
-
+        result=sum([tree.query(col2+1)-tree.query(col1) for tree in self.trees[row1:row2+1]])
+        return result
 
 # Your NumMatrix object will be instantiated and called as such:
 # obj = NumMatrix(matrix)
