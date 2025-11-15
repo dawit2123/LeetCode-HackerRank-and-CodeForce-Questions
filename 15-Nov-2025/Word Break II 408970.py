10895476afe5625e677bada874fe3934619a0d0c
Dawit Munie <106137900+dawit2123@users.noreply.github.com>
2025-11-15 14:34:02 -0500

    Created 'Word Break II' by A2SV Hub

---
+++ 15-Nov-2025/Word Break II 408970.py
@@ -0,0 +1,17 @@
+# Problem: Word Break II - https://leetcode.com/problems/word-break-ii/description/
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        word_dict=set(wordDict)
+        cur=[]
+        result=[]
+        def backtrack(i):
+            if i==len(s):
+                result.append(" ".join(cur))
+            for j in range(i, len(s)):
+                if s[i:j+1] in word_dict:
+                    cur.append(s[i:j+1])
+                    backtrack(j+1)
+                    cur.pop()
+        backtrack(0)
+        return result
