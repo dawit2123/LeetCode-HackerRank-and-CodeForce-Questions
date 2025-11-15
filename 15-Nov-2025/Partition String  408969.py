2116f38a73772ccb115f5c8552883857b8839692
Dawit Munie <106137900+dawit2123@users.noreply.github.com>
2025-11-15 14:33:53 -0500

    Created 'Partition String ' by A2SV Hub

---
+++ 15-Nov-2025/Partition String  408969.py
@@ -0,0 +1,10 @@
+# Problem: Partition String  - https://leetcode.com/problems/partition-string/description/
+
+class Solution:
+    def partitionString(self, s: str) -> List[str]:
+        seen,res,cur=set(),[],''   
+        for ch in s:
+            cur+=ch
+            if cur in seen: continue
+            seen.add(cur); res.append(cur); cur=''
+        return res
