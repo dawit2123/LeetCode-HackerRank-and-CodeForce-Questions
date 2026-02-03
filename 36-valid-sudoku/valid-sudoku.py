52985a0dc4137b75941137002324fcfa6ae47478
Dawit Munie <106137900+dawit2123@users.noreply.github.com>
2026-02-03 06:42:05 -0500

    Time: 3 ms (79.45%) | Memory: 19.5 MB (19.38%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -1,18 +1,19 @@
+from collections import defaultdict
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        rows= defaultdict(set)
-        cols= defaultdict(set)
-        sub_board= defaultdict(set)
-        for r in range(0,9):
-            for c in range(0,9):
-                if board[r][c]==".":
+        row_dict=defaultdict(set)
+        col_dict=defaultdict(set)
+        sub_box_dict=defaultdict(set)
+        for r in range(9):
+            for c in range(9):
+                num=board[r][c]
+                if num==".":
                     continue
-                elif (board[r][c] in rows[r] or board[r][c] in cols[c] or board[r][c] in sub_board[(r//3, c//3)]):
-                    return False
                 else:
-                    rows[r].add(board[r][c])
-                    cols[c].add(board[r][c])
-                    sub_board[(r//3,c//3)].add(board[r][c])
+                    if (num in row_dict[r]) or (num in col_dict[c]) or (num in sub_box_dict[(r//3, c//3)]):
+                        return False
+                    else:
+                        row_dict[r].add(num)
+                        col_dict[c].add(num)
+                        sub_box_dict[(r//3, c//3)].add(num)
         return True
-
-        
