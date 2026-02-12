55ff4c170217b75d4e73d7ecb4b44308a03beaa6
Dawit Munie <106137900+dawit2123@users.noreply.github.com>
2026-02-12 17:52:19 -0500

    Time: 0 ms (100.00%) | Memory: 19.4 MB (45.63%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -3,8 +3,11 @@ class Solution:
         hash_map={")":"(", "}":"{", "]":"["}
         stack=[]
         for c in s:
-            if stack and c in hash_map and stack[-1]==hash_map[c]:
-                stack.pop()
+            if stack and c in hash_map:
+                if stack and hash_map[c]==stack[-1]:
+                    stack.pop()
+                else:
+                    return False
             else:
                 stack.append(c)
         return len(stack)==0
