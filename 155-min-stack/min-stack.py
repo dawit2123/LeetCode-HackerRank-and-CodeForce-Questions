1fe0d48c2076ef2a4c1f1385715ec63786381877
Dawit Munie <106137900+dawit2123@users.noreply.github.com>
2026-02-13 18:16:48 -0500

    Time: 7 ms (49.18%) | Memory: 22.7 MB (20.26%) - LeetSync

--- 155-min-stack/min-stack.py
+++ 155-min-stack/min-stack.py
@@ -1,17 +1,17 @@
 class MinStack:
 
     def __init__(self):
-        self.stack=[]
+        self.stack=[]        
         self.min_stack=[]
 
     def push(self, val: int) -> None:
         self.stack.append(val)
-        self.min_stack.append(min(val, self.min_stack[-1] if self.min_stack else val))
+        min_val=min(self.min_stack[-1], val) if self.min_stack else val
+        self.min_stack.append(min_val)
 
     def pop(self) -> None:
         self.stack.pop()
         self.min_stack.pop()
-        
 
     def top(self) -> int:
         return self.stack[-1]
