542aae2244f331fa88a9090414025488ebf614f5
Dawit Munie <106137900+dawit2123@users.noreply.github.com>
2026-02-16 02:12:41 -0500

    Time: 9 ms (98.37%) | Memory: 19.3 MB (68.42%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -1,33 +1,17 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        if len(s1) > len(s2):
+        if len(s1)>len(s2):
             return False
-        s1_count, s2_count = [0]*26, [0]*26
-
-        for i in range(len(s1)):
-            s1_count[(ord(s1[i])-ord('a'))]+=1
-            s2_count[(ord(s2[i])-ord('a'))]+=1
-        
-        matches=0
-        for i in range(26):
-            if s1_count[i]==s2_count[i]:
-                matches+=1
-        l=0
-        for r in range(len(s1), len(s2)):
-            if matches==26: return True
-            index=ord(s2[r]) - ord('a')
-            s2_count[index]+=1
-            if s2_count[index]==s1_count[index]:
-                matches+=1
-            elif s1_count[index]+1==s2_count[index]:
-                matches-=1
-
-            index= ord(s2[l]) - ord('a')
-            s2_count[index]-=1
-            if s2_count[index]==s1_count[index]:
-                matches+=1
-            elif s1_count[index]-1 == s2_count[index]:
-                matches-=1
-            l+=1
-        return matches==26
-
+        s1_count, s2_count= [0]*26, [0]*26
+        for c in s1:
+            s1_count[ord(c)-ord('a')]+=1
+        left=0
+        for i in range(left, len(s1)-1):
+            s2_count[ord(s2[i])-ord('a')]+=1
+        for j in range(len(s1)-1, len(s2)):
+            s2_count[ord(s2[j])-ord('a')]+=1
+            if s2_count==s1_count:
+                return True
+            s2_count[ord(s2[left])-ord('a')]-=1
+            left+=1
+        return False
