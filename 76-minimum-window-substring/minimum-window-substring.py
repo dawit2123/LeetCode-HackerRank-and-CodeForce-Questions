4fdfd60c25252f8c12892a4daeec5eb05e6bff7a
Dawit Munie <106137900+dawit2123@users.noreply.github.com>
2026-02-17 05:11:31 -0500

    Time: 175 ms (18.47%) | Memory: 20.2 MB (9.96%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -1,29 +1,29 @@
+from collections import defaultdict, Counter
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        if len(s)<len(t) or t=="":
+        if len(t)==0:
             return ""
-        res, res_length=[-1, -1], float('inf')
-        freq_t, window_s={}, {}
-        for c in t:
-            freq_t[c]= 1+freq_t.get(c, 0)
-        
-        have, need=0, len(freq_t)
-        l=0
-        for r in range(len(s)):
-            c= s[r]
-            window_s[c]= 1+window_s.get(c, 0)
-            if c in freq_t and window_s[c]==freq_t[c]:
-                have+=1
+        t_count=Counter(t)
+        s_count=defaultdict(int)
+        have, need= 0, len(t_count)
+        min_left, min_right, min_window= 0, 0, float('infinity')
+        left=0
+        for right in range(len(s)):
+            c=s[right]
+            if c in t_count:
+                s_count[c]+=1
+                if s_count[c]==t_count[c]:
+                    have+=1
             while have==need:
-                # update the result
-                if r-l+1 < res_length:
-                    res=[l,r]
-                    res_length=r-l+1
-                # pop the left
-                window_s[s[l]]-=1
-                if s[l] in freq_t and window_s[s[l]]<freq_t[s[l]]:
-                    have-=1
-                l+=1
-        l,r=res
-        return s[l:r+1] if res_length!=float("infinity") else ""
-        
+                c=s[left]
+                print('hello', min_left, min_right)
+                if (right-left+1)<min_window:
+                    min_left, min_right, min_window= left, right, (right-left+1)
+                if c in t_count:
+                    s_count[c]-=1
+                    if s_count[c]<t_count[c]:
+                        have-=1
+                left+=1
+        return s[min_left:min_right+1] if min_window!=float('infinity') else ""
+
+
