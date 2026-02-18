08d674956ebb2caa51445976c74cb077c7dd78d7
Dawit Munie <106137900+dawit2123@users.noreply.github.com>
2026-02-18 06:02:35 -0500

    Time: 178 ms (69.19%) | Memory: 35.3 MB (47.47%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.py
+++ 239-sliding-window-maximum/sliding-window-maximum.py
@@ -3,14 +3,14 @@ class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
         output=[]
         l=0
-        q=deque()
+        q=deque() # store indices with their monotonically decreasing values
         for r in range(len(nums)):
-            while q and nums[q[-1]]<nums[r]:
+            while q and nums[r]>nums[q[-1]]:
                 q.pop()
             q.append(r)
             if l>q[0]:
                 q.popleft()
-            if r+1>=k:
+            if r>=k-1:
                 output.append(nums[q[0]])
                 l+=1
         return output
