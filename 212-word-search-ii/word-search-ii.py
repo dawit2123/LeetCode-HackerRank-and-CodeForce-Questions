71d127d42aa0a2a56cbcc34d50d517b34d852236
Dawit Munie <106137900+dawit2123@users.noreply.github.com>
2026-02-27 17:58:19 -0500

    Time: 6291 ms (29.32%) | Memory: 21.6 MB (30.38%) - LeetSync

--- 212-word-search-ii/word-search-ii.py
+++ 212-word-search-ii/word-search-ii.py
@@ -1,37 +1,49 @@
 class TrieNode:
     def __init__(self):
-        self.children={}
-        self.is_word=False
-    def add(self, word):
-        curr=self
-        for c in word:
-            if c not in curr.children:
-                curr.children[c]=TrieNode()
-            curr=curr.children[c]
-        curr.is_word=True
-class Solution:
-    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
-        root=TrieNode()
-        for word in words:
-            root.add(word)
-        ROWS, COLS= len(board), len(board[0])
-        result, path=set(),set()
-        def dfs(r,c,node, word):
-            if  (r<0 or c<0 or r>=ROWS or c>=COLS or (r,c) in path or board[r][c] not in node.children):
-                return
-            path.add((r,c))
-            node=node.children[board[r][c]]
-            word+=board[r][c]
-            if node.is_word:
-                result.add(word)
-            dfs(r+1,c,node, word)
-            dfs(r-1,c, node, word)
-            dfs(r, c+1, node, word)
-            dfs(r, c-1, node, word)
-            path.remove((r,c))
-        for R in range(ROWS):
-            for C in range(COLS):
-                dfs(R,C,root,"")
-        return list(result)
+        self.children = {}
+        self.is_end_of_word = False
 
-        
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word):
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+            node = node.children[char]
+        node.is_end_of_word = True
+
+class Solution:
+    def findWords(self, board, words):
+        trie = Trie()
+        for word in words:
+            trie.insert(word)
+
+        self.result = set()
+        self.rows, self.cols = len(board), len(board[0])
+        self.visited = set()
+
+        def backtrack(r, c, node, path):
+            if node.is_end_of_word:
+                self.result.add(path)
+                node.is_end_of_word = False 
+            
+            self.visited.add((r, c))
+            for dr, dc in [(0, 1), (1, 0), (0, -1), (-1, 0)]:  
+                new_r, new_c = r + dr, c + dc
+                if 0 <= new_r < self.rows and 0 <= new_c < self.cols and (new_r, new_c) not in self.visited:
+                    next_char = board[new_r][new_c]
+                    if next_char in node.children:
+                        backtrack(new_r, new_c, node.children[next_char], path + next_char)
+
+            self.visited.remove((r, c))
+
+        for r in range(self.rows):
+            for c in range(self.cols):
+                start_char = board[r][c]
+                if start_char in trie.root.children:
+                    backtrack(r, c, trie.root.children[start_char], start_char)
+
+        return list(self.result)
