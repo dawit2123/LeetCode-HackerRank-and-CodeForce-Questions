f45fe140b888334cf232d3c91271bf9205e4c187
Dawit Munie <106137900+dawit2123@users.noreply.github.com>
2026-04-11 12:15:35 -0400

    Time: 57 ms (89.72%) | Memory: 19.4 MB (34.20%) - LeetSync

--- 1580-shuffle-the-array/shuffle-the-array.py
+++ 1580-shuffle-the-array/shuffle-the-array.py
@@ -1,7 +1,16 @@
 class Solution:
     def shuffle(self, nums: List[int], n: int) -> List[int]:
-        numbers=[]
-        for i in range(0,n):
-            numbers.append(nums[i])
-            numbers.append(nums[i+n])
-        return numbers
+        # iterate over the value from 0, n and build the combination of x and y
+        for i in range(n):
+            x=nums[i]<<10
+            y=nums[i+n]
+            nums[i]=x|y
+        j=2*n-1
+        for k in range(n-1, -1, -1):
+            # extract the value of x and y
+            x=nums[k]>>10
+            y=nums[k]&((2**10)-1)
+            nums[j]=y
+            nums[j-1]=x
+            j-=2
+        return nums
