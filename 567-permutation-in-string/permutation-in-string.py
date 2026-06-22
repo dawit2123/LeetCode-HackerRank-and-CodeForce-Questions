f2c1a0af07df8284d81324eea89604e16dc7190a
Dawit Munie <106137900+dawit2123@users.noreply.github.com>
2026-06-22 07:55:45 -0400

    Time: 11 ms (96.82%) | Memory: 19.3 MB (53.23%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -5,13 +5,14 @@ class Solution:
         s1_count, s2_count= [0]*26, [0]*26
         for c in s1:
             s1_count[ord(c)-ord('a')]+=1
-        left=0
-        for i in range(left, len(s1)-1):
+        for i in range(len(s1)-1):
             s2_count[ord(s2[i])-ord('a')]+=1
-        for j in range(len(s1)-1, len(s2)):
-            s2_count[ord(s2[j])-ord('a')]+=1
+        left=0
+        for right in range(len(s1)-1, len(s2)):
+            s2_count[ord(s2[right])-ord('a')]+=1
             if s2_count==s1_count:
                 return True
-            s2_count[ord(s2[left])-ord('a')]-=1
-            left+=1
+            else:
+                s2_count[ord(s2[left])-ord('a')]-=1
+                left+=1
         return False
