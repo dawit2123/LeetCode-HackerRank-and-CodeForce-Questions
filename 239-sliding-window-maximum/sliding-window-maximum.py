881bf226cafbbd59b0d89849a74dcb860011a8dc
Dawit Munie <106137900+dawit2123@users.noreply.github.com>
2026-07-03 18:24:09 -0400

    Time: 331 ms (12.66%) | Memory: 46.6 MB (5.03%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.py
+++ 239-sliding-window-maximum/sliding-window-maximum.py
@@ -1,16 +1,12 @@
-from collections import deque
+import heapq
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        output=[]
-        l=0
-        q=deque() # store indices with their monotonically decreasing values
-        for r in range(len(nums)):
-            while q and nums[r]>nums[q[-1]]:
-                q.pop()
-            q.append(r)
-            if l>q[0]:
-                q.popleft()
-            if r>=k-1:
-                output.append(nums[q[0]])
-                l+=1
-        return output
+        result=[]
+        max_heap=[]
+        for i, num in enumerate(nums):
+            heapq.heappush(max_heap, (-1*num, i))
+            if i>=k-1:
+                while max_heap[0][1]<=i-k:
+                    heapq.heappop(max_heap)
+                result.append(-1*max_heap[0][0])
+        return result
