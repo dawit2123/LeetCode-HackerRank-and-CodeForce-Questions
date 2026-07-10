c10bde85c79dd8d8c59e73aedf1ce3ca077a73d0
Dawit Munie <106137900+dawit2123@users.noreply.github.com>
2026-07-10 07:32:57 -0400

    Time: 199 ms (34.49%) | Memory: 41.6 MB (43.79%) - LeetSync

--- 883-car-fleet/car-fleet.py
+++ 883-car-fleet/car-fleet.py
@@ -1,12 +1,12 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        car_info= [(p, s) for p, s in zip(position, speed)]
-        car_info.sort(reverse=True)
-        prev_time=(target-car_info[0][0])/car_info[0][1]
+        car_stat=[(p,s) for p,s in zip(position, speed)]
+        car_stat.sort()
+        prev_time=(target-car_stat[-1][0])/car_stat[-1][1]
         result=1
-        for i in range(1, len(car_info)):
-            time= (target-car_info[i][0])/car_info[i][1]
+        for i in range(len(car_stat)-2, -1, -1):
+            time=(target-car_stat[i][0])/car_stat[i][1]
             if time>prev_time:
+                prev_time=time
                 result+=1
-                prev_time=time        
         return result
