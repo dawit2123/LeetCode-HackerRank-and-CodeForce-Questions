23cf03c7bcb7c2a9c7753304f13e0a748942a284
Dawit Munie <106137900+dawit2123@users.noreply.github.com>
2026-09-13 17:18:29 -0400

    Time: 55 ms (57.27%) | Memory: 29.8 MB (54.48%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,49 +1,47 @@
 class Node:
     def __init__(self, key, value):
         self.key=key
-        self.val=value
+        self.value=value
         self.prev, self.next= None, None
 
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.hash_map={}
         self.capacity=capacity
+        self.hash_map={}
         self.left, self.right= Node(0, 0), Node(0, 0)
-        self.left.next, self.right.prev= self.right, self.left
-
-    def insert(self, node):
+        self.left.next, self.right.prev=self.right, self.left
+    
+    def add(self, node):
         prev, nxt= self.right.prev, self.right
-        prev.next=node
-        nxt.prev=node
-        node.next=nxt
-        node.prev=prev
+        prev.next=nxt.prev=node
+        node.prev, node.next= prev, nxt
+    
     def remove(self, node):
         prev, nxt= node.prev, node.next
-        prev.next= nxt
-        nxt.prev=prev
+        prev.next, nxt.prev= nxt, prev
+
     def get(self, key: int) -> int:
         if key in self.hash_map:
-            node= self.hash_map[key]
+            node=self.hash_map[key]
             self.remove(node)
-            self.insert(node)
-            return node.val
+            self.add(node)
+            return node.value
         return -1
+
 
     def put(self, key: int, value: int) -> None:
         if key in self.hash_map:
-            node= self.hash_map[key]
-            self.remove(node)
+            self.remove(self.hash_map[key])
             del self.hash_map[key]
-        new_node= Node(key, value)
-        self.insert(new_node)
-        self.hash_map[key]=new_node
+        node=Node(key, value)
+        self.hash_map[key]=node
+        self.add(node)
         if len(self.hash_map)>self.capacity:
-            node= self.left.next
+            node=self.left.next
             self.remove(node)
             del self.hash_map[node.key]
-        
-
+        print(f'capacity{self.capacity}, hash map {len(self.hash_map)}')
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
